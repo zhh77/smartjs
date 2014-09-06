0061bd4bd0a57077860ffa68b145bb66ed7780ad
zhh77 <zhh77_1999@163.com>
2014-09-06 20:25:07 +0800

    拆分datamanager

--- main-debug.js
+++ main-debug.js
@@ -7,7 +7,7 @@ define(function() {
         }
     });
 
-    var libs = ["lib/jquery", '../dest/smart.min','../dest/smart-dataManager.min'],
+    var libs = ["lib/jquery", '../dest/smart','../dest/smart-dataManager'],
         res = ['base/util', 'base/aop', 'base/oop',
                 'dataManager/filterBuilder', 'dataManager/dataManager', 'dataManager/dataManager-table'
               ],
